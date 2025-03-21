4303f378d0bad9d68e1f9e2c4672aed1d2f63829
ousmanelagrange <ousmanemfochive4@gmail.com>
2025-03-21 18:14:14 +0100

    modified:   recruitment_backend/recruitment_backend/settings.py

--- recruitment_backend/recruitment_backend/settings.py
+++ recruitment_backend/recruitment_backend/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  
+    'drf_yasg',
     'rest_framework_simplejwt', # activation du module de blaklist
     'rest_framework_simplejwt.token_blacklist',  
     'cv_upload',
